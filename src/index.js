020d0283e1c76e1542637a024033c39e9715cf4b
Canberk Demirkan <canberk.demirkan@mtc.berlin>
2018-12-13 18:02:24 +0100

    async loading with promises fixed

--- src/index.js
+++ src/index.js
@@ -9,48 +9,86 @@ import './styles/main.css';
 import './js/materialize.min.js';
 import SevdeskClient from './modules/sevdesk-client';
 
-const client = new SevdeskClient('depatech-schedule-201849.csv');
 
-client.loadTags().then(() => {
-  client.getContactIdByTagName('6542').then(res => {
-    console.log(res);
-  });
+const client = new SevdeskClient('depatech-schedule-201849.csv');
+const contactId = client.getContactIdByTagName('9874');
+
+contactId.then(value => {
+  client.showId(value);
 });
 
-const fData = {
-  'orderNumber': 'DE - 1010',
-  'contact[id]': '6659849',
-  'contact[objectName]': 'Contact',
-  'orderDate': '1544448937',
-  'status': '100',
-  'header': 'TEST Lieferschein DE - 1008',
-  'headText': '<p>Sehr geehrte Damen und Herren,</p> <p>vielen Dank für Ihre Anfrage. Gerne unterbreiten wir Ihnen das gewünschte freibleibende Angebot:</p>',
-  'footText': '<p>Für Rückfragen stehen wir Ihnen jederzeit gerne zur Verfügung.<br> Wir bedanken uns sehr für Ihr Vertrauen.</p><p>Mit freundlichen Grüßen<br>[%KONTAKTPERSON%]</p>',
-  'addressName': 'Muster GmbH',
-  'addressCountry[id]': 1,
-  'addressCountry[objectName]': 'StaticCountry',
-  'version': 0,
-  'smallSettlement': false,
-  'contactPerson[id]': '248855',
-  'contactPerson[objectName]': 'SevUser',
-  'taxRate': 0,
-  'taxText': 0,
-  'taxType': 'default',
-  'orderType': 'LI',
-  'address': 'Muster GmbH Musterstr. 1 11111 Berlin',
-  'currency': 'EUR',
-  'sumNet': 0,
-  'sumTax': 0,
-  'sumGross': 0,
-  'sumDiscounts': 0,
-  'sumNetForeignCurrency': 0,
-  'sumTaxForeignCurrency': 0,
-  'sumGrossForeignCurrency': 0,
-  'sumDiscountsForeignCurrency': 0,
-  'weight': 0,
-  'customerInternalNote': '1234',
-  'showNet': false,
-  'objectName': 'Order',
-  'discountTime': 0,
-  'discount': 0
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fData = {
+//   'orderNumber': 'DE - 1010',
+//   'contact[id]': '6659849',
+//   'contact[objectName]': 'Contact',
+//   'orderDate': '1544448937',
+//   'status': '100',
+//   'header': 'TEST Lieferschein DE - 1008',
+//   'headText': '<p>Sehr geehrte Damen und Herren,</p> <p>vielen Dank für Ihre Anfrage. Gerne unterbreiten wir Ihnen das gewünschte freibleibende Angebot:</p>',
+//   'footText': '<p>Für Rückfragen stehen wir Ihnen jederzeit gerne zur Verfügung.<br> Wir bedanken uns sehr für Ihr Vertrauen.</p><p>Mit freundlichen Grüßen<br>[%KONTAKTPERSON%]</p>',
+//   'addressName': 'Muster GmbH',
+//   'addressCountry[id]': 1,
+//   'addressCountry[objectName]': 'StaticCountry',
+//   'version': 0,
+//   'smallSettlement': false,
+//   'contactPerson[id]': '248855',
+//   'contactPerson[objectName]': 'SevUser',
+//   'taxRate': 0,
+//   'taxText': 0,
+//   'taxType': 'default',
+//   'orderType': 'LI',
+//   'address': 'Muster GmbH Musterstr. 1 11111 Berlin',
+//   'currency': 'EUR',
+//   'sumNet': 0,
+//   'sumTax': 0,
+//   'sumGross': 0,
+//   'sumDiscounts': 0,
+//   'sumNetForeignCurrency': 0,
+//   'sumTaxForeignCurrency': 0,
+//   'sumGrossForeignCurrency': 0,
+//   'sumDiscountsForeignCurrency': 0,
+//   'weight': 0,
+//   'customerInternalNote': '1234',
+//   'showNet': false,
+//   'objectName': 'Order',
+//   'discountTime': 0,
+//   'discount': 0
+// };
--- src/modules/sevdesk-client.js
+++ src/modules/sevdesk-client.js
@@ -1,6 +1,5 @@
 import { config } from './config';
 import Contact from './contact';
-// import _ from 'underscore';
 import _ from 'lodash';
 
 const isProdMode = config.env == 'production' ? true : false;
@@ -15,182 +14,182 @@ const options = {
 class SevdeskClient {
 
   constructor(file) {
+    this._cacheDomElements();
+    this._addListeners();
+
     this._file = file;
     this._api = isProdMode ? config.api : `http://${config.host}:${config.port}`;
-
-    this._createOrderLiBtn = document.querySelector('#create-order-li-btn');
-    this._resultDiv = document.querySelector('#api-result');
-    this.addListeners();
 
     this._tagMap = [];
     this._tagRelationMap = [];
     this._contacts = [];
-
-    console.log('LOG: constructor');
+    
+    console.log('LOG: end constructor');
   }
   
-  addListeners() {
+  _cacheDomElements() {
+    this._createOrderLiBtn = document.querySelector('#create-order-li-btn');
+    this._resultDiv = document.querySelector('#api-result');
+    console.log('LOG: _cacheDomElements');
+  }
+  
+  _addListeners() {
     this._createOrderLiBtn.addEventListener('click', e => this.createOrderLI(e));
-
-    console.log('LOG: addListeners');
+    console.log('LOG: _addListeners');
   }
 
-  async loadTags() {
+  async _loadTags() {
     let request = isProdMode ? new Request(`${this._api}/Tag`, options) : new Request(`${this._api}/Tag`);
 
-    await fetch(request)
-      .then(res => {
-        console.log('LOG: loadTags 1st then');
+    try {
+      const response = await fetch(request);
+      const json = await response.json();
 
-        return res.json();
-      })
-      .then(data => {
-        console.log('LOG: loadTags 2nd then');
-
-        let arr = [];
-        data.objects.forEach((item, key) => {
-          arr.push({
-            tagId: item.id,
-            tagName: item.name
-          });
+      let tags = [];
+      json.objects.forEach((item, index) => {
+        tags.push({
+          tagId: item.id,
+          tagName: item.name
         });
-        this.tagMap = arr;
-      })
-      .then(() => {
-        console.log('LOG: loadTags 3rd then');
-
-        this._loadTagRelations();
-      })
-      .catch(errorThrown => {
-        console.log('ERROR: ' + errorThrown);
       });
 
-    console.log('LOG: loadTags');
+      console.log('LOG: _loadTags');
+      // this._tagMap = tags;
+      return tags;
+
+    } catch (error) {
+      console.log('ERROR: ' + error);
+    }
   }
 
   async _loadTagRelations() {
     let request = isProdMode ? new Request(`${this._api}/TagRelation`, options) : new Request(`${this._api}/TagRelation`);
 
-    await fetch(request)
-      .then(res => {
-        console.log('LOG: _loadTagRelations 1st then');
-
-        return res.json();
-      })
-      .then(data => {
-        console.log('LOG: _loadTagRelations 2nd then');
-
-        let arr = [];
-        data.objects.forEach((item, key) => {
-          arr.push({
-            tagId: item.tag.id,
-            relContactId: item.object.id
-          });
+    try {
+      const response = await fetch(request);
+      const json = await response.json();
+      
+      let tagRelations = [];
+      json.objects.forEach((item, index) => {
+        tagRelations.push({
+          tagId: item.tag.id,
+          relContactId: item.object.id
         });
-        this.tagRelationMap = arr;
-      })
-      .then(() => {
-        console.log('LOG: _loadTagRelations 3rd then');
-
-        this._createContacts();
-      })
-      .catch(errorThrown => {
-        console.log('ERROR: ' + errorThrown);
       });
 
-    console.log('LOG: _loadTagRelations');
+      console.log('LOG: _loadTagRelations');
+      // this._tagRelationMap = tagRelations;
+      return tagRelations;  
+
+    } catch (error) {
+      console.log('ERROR: ' + error);
+    }
   }
 
-  // async createOrderLI(e) {
-  //   e.preventDefault();
-  //   let request = isProdMode ? new Request(`${this._api}/Order`, options) : new Request(`${this._api}/Order/${this._file}`);
+  async _createContacts() {
+    try {
+      const tags = await this._loadTags();
+      const tagRelations = await this._loadTagRelations();
 
-  //   await fetch(request)
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       this._resultDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-  //     })
-  //     .catch(errorThrown => {
-  //       console.log('ERROR: ' + errorThrown);
-  //     });
-  // }
+      let contacts = [];
+  
+      let merged = _.map(tags, item => {
+        return _.assign(item, _.find(tagRelations, ['tagId', item.tagId]));
+      });
+  
+      merged.forEach((item, index) => {
+        let contact = new Contact(item.relContactId, item.tagId, item.tagName);
+        contacts.push(contact);
+      });
+      
+      console.log('LOG: _createContacts');
+      // this._contacts = contacts;
+      return contacts;
 
-  _createContacts() {
-    /* underscore.js
-    let merged = _.map(this.tagMap, item => {
-      return _.extend(item, _.findWhere(this.tagRelationMap, { tagId: item.tagId }));
-    });
-    */
+    } catch (error) {
+      console.log('ERROR: ' + error);
+    }
+  }
 
-    /* lodash.js */
-    let merged = _.map(this.tagMap, item => {
-      return _.assign(item, _.find(this.tagRelationMap, ['tagId', item.tagId]));
-    });
+  async createOrderLI(e) {
+    e.preventDefault();
 
-    merged.forEach((item, key) => {
-      let contact = new Contact(item.relContactId, item.tagId, item.tagName);
-      this.contacts.push(contact);
-    });
+    let request = isProdMode ? new Request(`${this._api}/Order`, options) : new Request(`${this._api}/Order/${this._file}`);
 
-    console.log('LOG: _createContacts');
+    try {
+      const response = await fetch(request);
+      const json = await response;
+
+      this._resultDiv.innerHTML = `<pre>${JSON.stringify(json, null, 2)}</pre>`;
+      console.log('LOG: createOrderLI');
+
+    } catch (error) {
+      console.log('ERROR: ' + error);
+    }
   }
 
   async getContactIdByTagName(tagName) {
-    this.contacts.forEach((item, key) => {
-      if (tagName === item.tagName) {
-        return contact.id;
-      }
-    });
-
     console.log('LOG: getContactIdByTagName');
-  }
+    let contacts = await this._createContacts();
 
-  appendTagData() {
-    this.tagMap.forEach((tag, key) => {
-      let id = document.createTextNode(`ID: ${tag.tagId}\xa0`),
-          name = document.createTextNode(`Name: ${tag.tagName}`),
-          br = document.createElement('br'),
-          spanId = document.createElement('span'),
-          spanName = document.createElement('span');
-
-      spanId.appendChild(id);
-      spanName.appendChild(name);
-
-      this._resultDiv.appendChild(spanId);
-      this._resultDiv.appendChild(spanName);
-      this._resultDiv.appendChild(br);
+    let promise = new Promise((resolve, reject) => {
+      contacts.forEach((contact, index) => {
+        if (tagName === contact.tagName) {
+          resolve(contact.id);
+        }
+      });
     });
+
+    // promise.then((res) => {
+    // });
+
+    return promise;
   }
 
-  clearResultContainer() {
-    this._resultDiv.innerHTML = '';
+  showId(id) {
+    console.log(id);
   }
 
-  get tagMap() {
-    return this._tagMap;
-  }
+  // appendTagData() {
+  //   this.tagMap.forEach((tag, key) => {
+  //     let id = document.createTextNode(`ID: ${tag.tagId}\xa0`),
+  //         name = document.createTextNode(`Name: ${tag.tagName}`),
+  //         br = document.createElement('br'),
+  //         spanId = document.createElement('span'),
+  //         spanName = document.createElement('span');
 
-  set tagMap(tagMap) {
-    this._tagMap = tagMap;
-  }
+  //     spanId.appendChild(id);
+  //     spanName.appendChild(name);
 
-  get tagRelationMap() {
-    return this._tagRelationMap;
-  }
+  //     this._resultDiv.appendChild(spanId);
+  //     this._resultDiv.appendChild(spanName);
+  //     this._resultDiv.appendChild(br);
+  //   });
+  // }
 
-  set tagRelationMap(tagRelationMap) {
-    this._tagRelationMap = tagRelationMap;
-  }
+  // get tagMap() {
+  //   return this._tagMap;
+  // }
 
-  get contacts() {
-    return this._contacts;
-  }
+  // set tagMap(tagMap) {
+  //   this._tagMap = tagMap;
+  // }
 
-  set contacts(contact) {
-    this._contact = contact;
-  }
+  // get tagRelationMap() {
+  //   return this._tagRelationMap;
+  // }
+
+  // set tagRelationMap(tagRelationMap) {
+  //   this._tagRelationMap = tagRelationMap;
+  // }
+
+  // get contacts() {
+  //   return this._contacts;
+  // }
+
+  // set contacts(contact) {
+  //   this._contact = contact;
+  // }
 
 }
 
